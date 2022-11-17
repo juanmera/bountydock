5f3e51c8f0464ed0a65bfbbdec64d215a90a6905
Juan <juanmera@users.noreply.github.com>
2022-11-17 11:01:16 -0300

    Delete install_env.sh

--- install_env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-curl -sfL git.io/antibody | sh -s - -b /usr/local/bin
-antibody bundle < ~/.zshplugins.txt > ~/.zshplugins
-
-mkdir -p ~/.vim/bundle
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > ~/.vim/installer_dein.sh
-sh ~/.vim/installer_dein.sh ~/.vim/bundle
